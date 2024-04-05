ac61e1a380e6c5e228617dc97b6bb729e173e51b
K.V.N.Ramesh <kvn.ramesh@gmail.com>
2024-04-06 00:00:23 +0530

    server created

---
+++ src/app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const dotenv = require('dotenv');
+
+// Setting up config.env file variables
+dotenv.config({path : './config/config.env'});
+
+// Importing all routes
+const beams = require('./routes/beam.Routes');
+const shear = require('./routes/shear.Routes');
+
+app.use('/api/v1',beams);
+app.use('/api/v1',shear);
+
+const PORT = process.env.PORT;
+app.listen(PORT, () => {
+    console.log(`Server started on port ${process.env.PORT} in ${process.env.NODE_ENV} mode.`);
+});
