202a143fa1adb6f2ff0326aee24cc5cb19ce74fc
K.V.N.Ramesh <kvn.ramesh@gmail.com>
2024-04-22 01:44:49 +0530

    doubly reinforced

--- src/app.js
+++ src/app.js
@@ -8,9 +8,11 @@ dotenv.config({path : './config/config.env'});
 // Importing all routes
 const beams = require('./routes/beam.Routes');
 const shear = require('./routes/shear.Routes');
+const doublyBeam = require('./routes/doublyBeam.Routes');
 
 app.use('/api/v1',beams);
 app.use('/api/v1',shear);
+app.use('/api/v1',doublyBeam);
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
